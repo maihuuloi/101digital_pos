54eb0abe4f2215af4e470aa74625e5d3caf3d5e1
loimai <huuloiofficial@gmail.com>
2025-04-04 10:53:53 +0700

    chore: initialize project with basic structure and configuration

---
+++ src/test/java/com/digital/pos/ProcessOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessOrderServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
